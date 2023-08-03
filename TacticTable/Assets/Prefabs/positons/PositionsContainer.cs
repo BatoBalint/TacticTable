a7fcfcf591b081b84140f923b93e73160c408d66
Farkas08 <bercif.android@gmail.com>
2023-08-03 17:41:23 +0200

    positioning 6-os only

---
+++ TacticTable/Assets/Prefabs/positons/PositionsContainer.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PositionsContainer : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> korongok;
+    private List<Vector3> targetPositions;
+
+    private float slideDuration = 1f; // Time in seconds to complete the slide
+
+    private void Start()
+    {
+        targetPositions = new List<Vector3>();
+
+        // Populate the targetPositions with child GameObject positions
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            targetPositions.Add(transform.GetChild(i).position);
+        }
+    }
+
+    public void StartSlideRings()
+    {
+        StartCoroutine(SlideRings());
+    }
+
+    private System.Collections.IEnumerator SlideRings()
+    {
+        float t = 0f;
+        Vector3[] startPositions = new Vector3[korongok.Count];
+
+        // Store the starting positions of all objects
+        for (int i = 0; i < korongok.Count; i++)
+        {
+            startPositions[i] = korongok[i].transform.position;
+        }
+
+        // Move the objects simultaneously
+        while (t < 1f)
+        {
+            t += Time.deltaTime / slideDuration;
+            for (int i = 0; i < korongok.Count; i++)
+            {
+                korongok[i].transform.position = Vector3.Lerp(startPositions[i], targetPositions[i], t);
+            }
+            yield return null;
+        }
+
+        // Ensure all objects reach their exact target positions at the end
+        for (int i = 0; i < korongok.Count; i++)
+        {
+            korongok[i].transform.position = targetPositions[i];
+        }
+    }
+}
