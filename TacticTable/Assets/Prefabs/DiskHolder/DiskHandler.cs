f0f422ae727bfecda8e7a45ae69d7423c6c62504
BatoBalint <balint.bato@gmail.com>
2023-08-21 23:51:36 +0200

    background and button responsiveness improved, added players and ball

--- TacticTable/Assets/Prefabs/DiskHolder/DiskHandler.cs
+++ TacticTable/Assets/Prefabs/DiskHolder/DiskHandler.cs
@@ -1,7 +1,4 @@
-using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.U2D.Animation;
 using UnityEngine;
 
 public class DiskHandler : MonoBehaviour
