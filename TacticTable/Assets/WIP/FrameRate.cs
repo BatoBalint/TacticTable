0c81b6d78375cbb50df545fe9b3a26963bd90b29
BatoBalint <balint.bato@gmail.com>
2023-08-30 00:09:44 +0200

    animation scene added, no functions yet

--- TacticTable/Assets/WIP/FrameRate.cs
+++ TacticTable/Assets/WIP/FrameRate.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class FrameRate : MonoBehaviour
 {
+    // On start sets the framerate to 60
+    // Otherwise its locked to some lower value and its just feels bad
+    // Should be atached to something on the first scene
     void Awake()
     {
-        Application.targetFrameRate = 60;       
+        Application.targetFrameRate = 60;
     }
 }
