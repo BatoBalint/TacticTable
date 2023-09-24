b56ef6bead0ceaf13b9f818e9077fe68232b5aa2
Farkas08 <bercif.android@gmail.com>
2023-09-24 11:16:19 +0200

    position + scale

--- TacticTable/Assets/Prefabs/positons/PositionsContainer.cs
+++ TacticTable/Assets/Prefabs/positons/PositionsContainer.cs
@@ -5,54 +5,56 @@ using UnityEngine.UI;
 public class PositionsContainer : MonoBehaviour
 {
     [SerializeField]
-    private List<GameObject> korongok;
-    private List<Vector3> targetPositions;
+    private Transform BlueDisksHolder;
+    [SerializeField]
+    private Transform RedDisksHolder;
+
 
     private float slideDuration = 1f; // Time in seconds to complete the slide
 
-    private void Start()
-    {
-        targetPositions = new List<Vector3>();
-    }
 
-    private void SetTargetPositions()
-    {
-        targetPositions.Clear();
-
-        // Populate the targetPositions with child GameObject positions
-        for (int i = 0; i < transform.childCount; i++)
-        {
-            targetPositions.Add(new Vector3(transform.GetChild(i).position.x, transform.GetChild(i).position.y, 0f));
-        }
-    }
-
-    public void StartSlideRings()
-    {
-        SetTargetPositions();
-        StartCoroutine(SlideRings());
-    }
-
-    private System.Collections.IEnumerator SlideRings()
-    {
+    private System.Collections.IEnumerator Move(int index)
+     {
         float t = 0f;
-        Vector3[] startPositions = new Vector3[korongok.Count];
+        
 
-        // Store the starting positions of all objects
-        for (int i = 0; i < korongok.Count; i++)
-        {
-            startPositions[i] = korongok[i].transform.position;
-        }
+         Transform redPositions = transform.GetChild(index).GetChild(0);
+        
+         Transform bluePositions = transform.GetChild(index).GetChild(1);
 
-        // Move the objects simultaneously
         while (t < 1f)
         {
             t += Time.deltaTime / slideDuration;
-            for (int i = 0; i < korongok.Count; i++)
+            for (int i = 0; i < redPositions.childCount && i < RedDisksHolder.childCount && i < bluePositions.childCount && i < BlueDisksHolder.childCount; i++)
             {
-                korongok[i].transform.position = Vector3.Lerp(startPositions[i], targetPositions[i], t);
+                BlueDisksHolder.GetChild(i).position = Vector3.Lerp(BlueDisksHolder.GetChild(i).position, bluePositions.GetChild(i).position,t);
+                
+                RedDisksHolder.GetChild(i).position = Vector3.Lerp(RedDisksHolder.GetChild(i).position, redPositions.GetChild(i).position,t);
             }
-            
+
             yield return null;
         }
+    }
+
+
+     public void StartSix()
+     {
+        StopAllCoroutines();
+        StartCoroutine(Move(0));
+     }
+    public void StartFiveOne()
+    {
+        StopAllCoroutines();
+        StartCoroutine(Move(1));
+    }
+    public void StartFourTwo()
+    {
+        StopAllCoroutines();
+        StartCoroutine(Move(2));
+    }
+    public void StartThreeTwoOne()
+    {
+        StopAllCoroutines();
+        StartCoroutine(Move(3));
     }
 }
