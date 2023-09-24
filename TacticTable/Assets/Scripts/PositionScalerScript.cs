aabca22084a282d8845a1c4dd493c8f9d47705eb
Farkas08 <bercif.android@gmail.com>
2023-09-24 11:31:26 +0200

    hardcode removed

--- TacticTable/Assets/Scripts/PositionScalerScript.cs
+++ TacticTable/Assets/Scripts/PositionScalerScript.cs
@@ -34,16 +34,13 @@ public class PositionScalerScript : MonoBehaviour
         for (int i = 0; i < redPositions.childCount && i < _redDisks.childCount; i++)
         {
             _redDisks.GetChild(i).position = redPositions.GetChild(i).position;
+            _blueDisks.GetChild(i).position = bluePositions.GetChild(i).position;
         }
 
         if (bluePositions.childCount < 6 || _blueDisks.childCount < 6)
             return;
 
-        // It's hardcoded but currently I don't want to rewrite everything else
-        for (int i = 0; i < 6; i++)
-        {
-            _blueDisks.GetChild(5 - i).position = bluePositions.GetChild(i).position;
-        }
+
     }
 
     private void AdditionalDisksToStartingPosition()
